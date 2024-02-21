4991f71c67f0581040c5f913962bf23b69a607c5
Gabrielli Oliveira <33656144+gabrielliosc@users.noreply.github.com>
2024-02-20 22:38:32 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ import { Router } from "./router.js"
 
 const router = new Router()
 
-router.add("/SPA-Universe/", "/pages/home.html")
-router.add("/SPA-Universe/universo/", "/pages/universo.html")
-router.add("/SPA-Universe/exploracao/", "/pages/exploracao.html")
+router.add("/SPA-Universe/", "/SPA-Universe/pages/home.html")
+router.add("/SPA-Universe/universo/", "/SPA-Universe/pages/universo.html")
+router.add("/SPA-Universe/exploracao/", "/SPA-Universe/pages/exploracao.html")
 router.add(404, "/pages/404.html")
 
 router.handle()
